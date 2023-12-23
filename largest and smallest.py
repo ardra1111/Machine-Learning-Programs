d1c7e8e77388e01954d390547e7687ed6a5c611d
ardra1111 <141385513+ardra1111@users.noreply.github.com>
2023-12-23 14:53:40 +0530

    Add files via upload

---
+++ largest and smallest.py
@@ -0,0 +1,20 @@
+a = int( input("Enter the first number:"))
+b= int ( input ("Enter the second number:"))
+c= int ( input ("Enter the third number:"))
+d= int (input ("Enter the fourth number:"))
+if (a>b and a>c and a>d):
+	 print(a,"is the largest number\n")
+if (b>c and b>a and b>d):
+	 print(b,"is the largest number\n")
+if (c>d and c>a and c>b):
+	 print(c,"is the largest number\n")
+if (d>b and d>a and d>c):
+	 print(d,"is the largest number\n")
+if(a<b and a<c and a<d):
+	 print(a,"is the smallest number\n")
+if (b<c and b<d and b<a):
+	 print(b,"is the smallest number\n")
+if (c<a and c<d and c<b):
+	 print(c,"is the smallest number\n")
+if (d<a and d<b and d<c):
+	 print(d,"is the smallest number\n")
